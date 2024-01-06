dcb72cb7d93b0e391ef904bcec970db080fc8b2e
Fauzan Abrar <fauzanabrar24@gmail.com>
2024-01-07 01:41:53 +0800

    remove unused props

--- components/dialog-item-restrict.tsx
+++ components/dialog-item-restrict.tsx
@@ -1,25 +1,43 @@
-import React from 'react';
-import {DialogClose, DialogFooter, DialogTitle, DialogTrigger} from "@/components/ui/dialog";
-import {Select, SelectContent, SelectItem, SelectTrigger, SelectValue} from "@/components/ui/select";
-import {Separator} from "@/components/ui/separator";
-import {Input} from "@/components/ui/input";
+import React from "react";
+import {
+  DialogClose,
+  DialogFooter,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import { Separator } from "@/components/ui/separator";
+import { Input } from "@/components/ui/input";
 import Loading from "@/components/loading";
-import {Button} from "@/components/ui/button";
-import {LucidePlus} from "lucide-react";
-import {DialogItem} from "@/components/dialog-item";
+import { Button } from "@/components/ui/button";
+import { LucidePlus } from "lucide-react";
+import { DialogItem } from "@/components/dialog-item";
 
 type Props = {
-  handleDialogItemSelect: () => void
-  handleDialogItemOpenChange: (open: boolean) => void
-  setRestrictSelected: (bool: boolean) => void
-  restrictSelected: boolean
-  inputEmail: string
-  setInputEmail: (email: string) => void
-  handleRestrict: () => Promise<void>
-  loadingRestrict: boolean
-}
+  handleDialogItemSelect: () => void;
+  handleDialogItemOpenChange: (open: boolean) => void;
+  setRestrictSelected: (bool: boolean) => void;
+  restrictSelected: boolean;
+  inputEmail: string;
+  setInputEmail: (email: string) => void;
+  handleAddWhitelist: () => Promise<void>;
+};
 
-const DialogItemRestrict = ({handleDialogItemSelect, handleDialogItemOpenChange, setRestrictSelected, restrictSelected, handleRestrict, setInputEmail, loadingRestrict, inputEmail}: Props) => {
+const DialogItemRestrict = ({
+  handleDialogItemSelect,
+  handleDialogItemOpenChange,
+  setRestrictSelected,
+  restrictSelected,
+  handleAddWhitelist,
+  setInputEmail,
+  inputEmail,
+}: Props) => {
   return (
     <DialogItem
       triggerChildren={
@@ -33,14 +51,16 @@ const DialogItemRestrict = ({handleDialogItemSelect, handleDialogItemOpenChange,
     >
       <DialogTitle>Restrict This File</DialogTitle>
       <div className="grid gap-4 py-1">
-        <div className={'flex gap-4 items-center justify-between'}>
-          <p className={'font-medium text-sm'}>Access</p>
-          <Select onValueChange={(value) => {
-            if (value === "Public") setRestrictSelected(false)
-            else setRestrictSelected(true)
-          }}>
-            <SelectTrigger className={'w-24'}>
-              <SelectValue placeholder={"Public"}/>
+        <div className={"flex gap-4 items-center justify-between"}>
+          <p className={"font-medium text-sm"}>Access</p>
+          <Select
+            onValueChange={(value) => {
+              if (value === "Public") setRestrictSelected(false);
+              else setRestrictSelected(true);
+            }}
+          >
+            <SelectTrigger className={"w-24"}>
+              <SelectValue placeholder={"Public"} />
             </SelectTrigger>
             <SelectContent>
               <SelectItem value={"Public"}>Public</SelectItem>
@@ -50,8 +70,8 @@ const DialogItemRestrict = ({handleDialogItemSelect, handleDialogItemOpenChange,
         </div>
         {restrictSelected && (
           <>
-            <Separator/>
-            <span className={'font-bold text-sm'}>Add whitelist</span>
+            <Separator />
+            <span className={"font-bold text-sm"}>Add whitelist</span>
             <div className="flex items-center gap-2">
               <Input
                 id="name"
@@ -63,16 +83,15 @@ const DialogItemRestrict = ({handleDialogItemSelect, handleDialogItemOpenChange,
               />
               <DialogTrigger asChild>
                 <div className="flex gap-2 sm:flex-col sm:gap-4">
-                  {loadingRestrict && <Loading loading={loadingRestrict} size={30}/>}
                   <Button
                     variant={"outline"}
                     className="px-2"
                     type="submit"
                     onClick={async () => {
-                      await handleRestrict();
+                      await handleAddWhitelist();
                     }}
                   >
-                    <LucidePlus className={"w-4 mr-1"}/>
+                    <LucidePlus className={"w-4 mr-1"} />
                     Add
                   </Button>
                 </div>
@@ -83,11 +102,7 @@ const DialogItemRestrict = ({handleDialogItemSelect, handleDialogItemOpenChange,
       </div>
       <DialogFooter>
         <DialogClose asChild={true}>
-          <Button
-            variant={"default"}
-          >
-            Done
-          </Button>
+          <Button variant={"default"}>Done</Button>
         </DialogClose>
       </DialogFooter>
     </DialogItem>
