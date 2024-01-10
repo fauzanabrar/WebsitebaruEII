a0e27d5af5b5e4476f946b0ec385e99cd432db00
Fauzan Abrar <fauzanabrar24@gmail.com>
2024-01-11 00:40:47 +0800

    set useSession to required

--- components/menu.tsx
+++ components/menu.tsx
@@ -1,33 +1,35 @@
 "use client";
-import {Menubar} from "@/components/ui/menubar";
+import { Menubar } from "@/components/ui/menubar";
 
-import {signOut, useSession} from "next-auth/react";
+import { signOut, useSession } from "next-auth/react";
 import Link from "next/link";
-import {useEffect} from "react";
-import {LucideMenu, LucideSettings, LucideUser} from "lucide-react";
-import {Button} from "@/components/ui/button";
-import {usePathname} from "next/navigation";
-import {UserSession} from "@/lib/next-auth/auth";
-import {Sheet, SheetContent, SheetTrigger} from "@/components/ui/sheet";
-import {TokensIcon} from "@radix-ui/react-icons";
+import { useEffect, useMemo } from "react";
+import { LucideMenu, LucideSettings, LucideUser } from "lucide-react";
+import { Button } from "@/components/ui/button";
+import { usePathname } from "next/navigation";
+import { UserSession } from "@/lib/next-auth/auth";
+import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
+import { TokensIcon } from "@radix-ui/react-icons";
 import LogoutButton from "@/components/logout-button";
 
 export function Menu() {
-  const {data} = useSession();
-  const sessionUser = data as UserSession;
+  const { data } = useSession({ required: true });
+
+  const sessionUser = useMemo(() => data as UserSession, [data]);
   const pathname = usePathname();
   const activePath = pathname.split("/")[1];
 
-  useEffect(() => {
-  }, [data]);
+  useEffect(() => {}, [data]);
 
   return (
-    <Menubar
-      className="flex items-center justify-between lg:justify-end rounded-none border-b border-none px-2 lg:px-4">
+    <Menubar className="flex items-center justify-between lg:justify-end rounded-none border-b border-none px-2 lg:px-4">
       <div className={"block lg:hidden"}>
         <Sheet>
-          <SheetTrigger className={" flex items-center justify-center align-middle"}><LucideMenu
-            className={"ml-1 h-6 w-6"}/></SheetTrigger>
+          <SheetTrigger
+            className={" flex items-center justify-center align-middle"}
+          >
+            <LucideMenu className={"ml-1 h-6 w-6"} />
+          </SheetTrigger>
           <SheetContent side={"left"} className={"w-80 pt-10"}>
             {sessionUser?.user.role === "admin" && (
               <div>
@@ -36,15 +38,22 @@ export function Menu() {
                 </h2>
                 <div className="space-y-1">
                   <Link href={"/user"}>
-                    <Button variant={activePath === "user" ? "secondary" : "ghost"} className="w-full justify-start">
-                      <LucideUser className="mr-2 h-4 w-4"/>
+                    <Button
+                      variant={activePath === "user" ? "secondary" : "ghost"}
+                      className="w-full justify-start"
+                    >
+                      <LucideUser className="mr-2 h-4 w-4" />
                       User
                     </Button>
                   </Link>
                   <Link href={"/settings"}>
-                    <Button variant={activePath === "settings" ? "secondary" : "ghost"}
-                            className="w-full justify-start">
-                      <LucideSettings className="mr-2 h-4 w-4"/>
+                    <Button
+                      variant={
+                        activePath === "settings" ? "secondary" : "ghost"
+                      }
+                      className="w-full justify-start"
+                    >
+                      <LucideSettings className="mr-2 h-4 w-4" />
                       Settings
                     </Button>
                   </Link>
@@ -57,18 +66,24 @@ export function Menu() {
             </h2>
             <div className="space-y-1">
               <Link href={"/list"}>
-                <Button variant={activePath === "list" ? "secondary" : "ghost"} className="w-full justify-start">
-                  <TokensIcon className="mr-2 h-4 w-4"/>
+                <Button
+                  variant={activePath === "list" ? "secondary" : "ghost"}
+                  className="w-full justify-start"
+                >
+                  <TokensIcon className="mr-2 h-4 w-4" />
                   List
                 </Button>
               </Link>
               <Link href={"/settings"}>
-                <Button variant={activePath === "settings" ? "secondary" : "ghost"} className="w-full justify-start">
-                  <LucideSettings className="mr-2 h-4 w-4"/>
+                <Button
+                  variant={activePath === "settings" ? "secondary" : "ghost"}
+                  className="w-full justify-start"
+                >
+                  <LucideSettings className="mr-2 h-4 w-4" />
                   Settings
                 </Button>
               </Link>
-              <LogoutButton/>
+              <LogoutButton />
             </div>
           </SheetContent>
         </Sheet>
@@ -90,7 +105,10 @@ export function Menu() {
             <Link href="/login" className="px-2 py-1.5 text-sm font-semibold">
               Login
             </Link>
-            <Link href="/register" className="px-2 py-1.5 text-sm font-semibold">
+            <Link
+              href="/register"
+              className="px-2 py-1.5 text-sm font-semibold"
+            >
               Register
             </Link>
           </>
