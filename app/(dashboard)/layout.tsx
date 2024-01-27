e2537ace6fab75be86e230d485e58b858e84b747
Fauzan Abrar <fauzanabrar24@gmail.com>
2024-01-28 00:05:43 +0800

    add sidebar

---
+++ app/(dashboard)/layout.tsx
@@ -0,0 +1,26 @@
+import { Menu } from "@/components/menu";
+import { Sidebar } from "@/components/sidebar/sidebar";
+import { getUserSession } from "@/lib/next-auth/user-session";
+
+import { ReactNode } from "react";
+
+export default async function DashboardLayout({
+  children,
+}: {
+  children: ReactNode;
+}) {
+  const userSession = await getUserSession();
+  return (
+    <>
+      <div className="border-t">
+        <div className="bg-background">
+          <div className="block lg:grid lg:grid-cols-5">
+            <Sidebar userSession={userSession} />
+
+            <div className="lg:pt-4 lg:col-span-4">{children}</div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
